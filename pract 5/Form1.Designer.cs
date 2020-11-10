09e5297791fc5b60403cf4d5c74b4f9e5b76f45c
KoteAnimen <sverh.il@yandex.ru>
2020-11-10 09:55:32 +0300

    Тест

--- pract 5/Form1.Designer.cs
+++ pract 5/Form1.Designer.cs
@@ -284,7 +284,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Практическая №5";
+            this.Text = "Практическая №6";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
